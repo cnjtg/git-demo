b04202bb09f2bae7fd196d20fd26c980ed4e85bf
cnjtg <cnjtg_mx@163.com>
2021-05-07 21:07:46 +0800

    添加热部署插件

--- git-springboot/src/main/java/com/cnjtg/gitspringboot/beans/User.java
+++ git-springboot/src/main/java/com/cnjtg/gitspringboot/beans/User.java
@@ -1,7 +1,6 @@
 package com.cnjtg.gitspringboot.beans;
 
 import lombok.Data;
-import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
