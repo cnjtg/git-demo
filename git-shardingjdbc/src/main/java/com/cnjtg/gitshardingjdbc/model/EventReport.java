4aa4e1c588f0fbd88ffef1c1e4cc0d0f04fc3027
cnjtg <cnjtg_mx@163.com>
2023-02-08 16:54:13 +0800

    sharding-jdbc

---
+++ git-shardingjdbc/src/main/java/com/cnjtg/gitshardingjdbc/model/EventReport.java
@@ -0,0 +1,12 @@
+package com.cnjtg.gitshardingjdbc.model;
+
+import lombok.Data;
+
+/**
+ * @author cnjtg
+ * @slogan study hard and make progress every day
+ * @date 2023-02-08 16:49
+ */
+@Data
+public class EventReport {
+}
---
+++ git-shardingjdbc/src/test/java/com/cnjtg/gitshardingjdbc/GitShardingjdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnjtg.gitshardingjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitShardingjdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
